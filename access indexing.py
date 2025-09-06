493c0c80e93d945dd2f95cdab5987ac557d57ff9
sonu kushwaha <sonukuswhaha@gmail.com>
2025-09-07 00:42:41 +0530

    access indexing.py

---
+++ access indexing.py
@@ -0,0 +1,18 @@
+
+#---->+ l to r +ve
+#<------ r to l  -ve
+#last element - se start
+
+
+
+#1D
+#array[index]----->
+
+#2D
+#array[row,column]
+
+
+import numpy as np
+arr = np.array([10,20,30,40,50])
+print(arr[3])
+print(arr[-4])
